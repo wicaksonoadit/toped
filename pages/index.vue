1c37d4d58c57ae2e7d38ea007bb10b745e21545c
wicaksonoadit <wicaksonoadit@garena.co.id>
2018-09-06 17:14:25 +0700

    simplify method

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,10 @@
       <div class="input-group">
         <input type="text" placeholder="Input your number">
         <div class="btn btn-primary" @click="getAmount">
-          BUTTON
+          CEK AMOUNT
+        </div>
+        <div class="btn btn-primary" @click="getRealNum">
+          VALIDATION
         </div>
       </div>
 
@@ -21,8 +24,20 @@ export default {
     appHeader
   },
   methods: {
-    getRealNum: function (num) {
-
+    getRealNum: function (no) {
+      const regex = /^(rp\.? ?)*(([0-9]+\.*)+)+(,?[0]{2})?$/gmi
+      const str = 'Rp.15000'
+      console.log(regex.exec(str)[2].replace(/[^0-9 ]/g, ""))
+    },
+    rupiahCurrency: function (angka) {
+      var rupiah = '';
+      var angkarev = angka.toString().split('').reverse().join('');
+      for(var i = 0; i < angkarev.length; i++) {
+        if(i%3 == 0) {
+          rupiah += angkarev.substr(i,3)+'.'
+        }
+      }
+      return('Rp. '+rupiah.split('',rupiah.length-1).reverse().join(''))
     },
     getAmount: function (num) {
 
